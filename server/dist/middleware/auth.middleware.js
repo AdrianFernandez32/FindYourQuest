e22c9fda94ddde095970d8b5e742ac23eaad7fb7
Adrian Fernandez <adrianfersa@meta.com>
2023-07-20 13:11:26 -0700

    improved the verifyToken

--- server/dist/middleware/auth.middleware.js
+++ server/dist/middleware/auth.middleware.js
@@ -4,14 +4,29 @@ const verifyToken = (req, res, next) => {
     if (!authHeader) {
         return res.status(401).json({ message: "Unauthorized" });
     }
-    const token = authHeader.split(" ")[1];
+    const parts = authHeader.split(" ");
+    if (parts.length !== 2) {
+        return res.status(401).json({ message: "Token error" });
+    }
+    const [scheme, token] = parts;
+    if (!/^Bearer$/i.test(scheme)) {
+        return res.status(401).json({ message: "Token malformatted" });
+    }
     try {
-        const decoded = jwt.verify(token, "secretkey");
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
         next();
     }
     catch (error) {
-        return res.status(401).json({ message: "Invalid token" });
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(401).json({ message: "Invalid token" });
+        }
+        else if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: "Expired token" });
+        }
+        else {
+            return res.status(500).json({ message: "Internal server error" });
+        }
     }
 };
 export default verifyToken;
--- server/src/middleware/auth.middleware.ts
+++ server/src/middleware/auth.middleware.ts
@@ -18,14 +18,30 @@ const verifyToken = (
     return res.status(401).json({ message: "Unauthorized" });
   }
 
-  const token = authHeader.split(" ")[1];
+  const parts = authHeader.split(" ");
+
+  if (parts.length !== 2) {
+    return res.status(401).json({ message: "Token error" });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return res.status(401).json({ message: "Token malformatted" });
+  }
 
   try {
-    const decoded = jwt.verify(token, "secretkey") as TokenPayload;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as TokenPayload;
     req.user = decoded.user;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
+    if (error instanceof jwt.JsonWebTokenError) {
+      return res.status(401).json({ message: "Invalid token" });
+    } else if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).json({ message: "Expired token" });
+    } else {
+      return res.status(500).json({ message: "Internal server error" });
+    }
   }
 };
 
